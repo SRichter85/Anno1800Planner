44b0021c9ceef71d18fd04191b1298cd8514def5
S. Richter <sergei.richter.jr@gmail.com>
2025-06-12 02:16:06 +0200

    Some changes to production chain view, making @available work

---
+++ Anno1800Planner/Common/Converter/StringTruncateConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Anno1800Planner.Common
+{
+    public class StringTruncateConverter : IValueConverter
+    {
+        public int MaxLength { get; set; } = 6; // Default max length
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str)
+            {
+                return str.Length <= MaxLength ? str : str.Substring(0, MaxLength);
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Not needed for one-way binding
+            throw new NotImplementedException();
+        }
+    }
+}
--- Anno1800Planner/obj/Debug/net8.0-windows/Anno1800Planner.AssemblyInfo.cs
+++ Anno1800Planner/obj/Debug/net8.0-windows/Anno1800Planner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Anno1800Planner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b07418abd361a26807fe3fe0c4b55240bf024761")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e78e1f0a304c5665c883c16d4ef3e6e75a382a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Anno1800Planner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Anno1800Planner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
